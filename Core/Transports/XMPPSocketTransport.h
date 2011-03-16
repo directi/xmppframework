633457f89392730c1d4dbf430ee6cc5b134b06e2
Chaitanya Gupta <chaitanya.g@directi.com>
2011-03-16 17:15:52 +0530

    Added socket transport stub file.

---
+++ Core/Transports/XMPPSocketTransport.h
@@ -0,0 +1,16 @@
+//
+//  XMPPSocketTransport.h
+//  iPhoneXMPP
+//
+//  Created by Chaitanya Gupta on 16/03/11.
+//  Copyright 2011 Directi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface XMPPSocketTransport : NSObject {
+    
+}
+
+@end
---
+++ Core/Transports/XMPPSocketTransport.m
@@ -0,0 +1,14 @@
+//
+//  XMPPSocketTransport.m
+//  iPhoneXMPP
+//
+//  Created by Chaitanya Gupta on 16/03/11.
+//  Copyright 2011 Directi. All rights reserved.
+//
+
+#import "XMPPSocketTransport.h"
+
+
+@implementation XMPPSocketTransport
+
+@end
